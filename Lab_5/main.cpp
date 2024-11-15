f4698983623590d158d134b8293ea2117b892d2a
ZeroXMT <bertim1243@gmail.com>
2024-11-15 19:25:35 +0300

    Lab5 updated test and main + readme

--- Lab_5/main.cpp
+++ Lab_5/main.cpp
@@ -1,25 +1,61 @@
 #include "./include/Stack.hpp"
 #include "./include/MemoryResource.hpp"
 #include <iostream>
+#include <string>
+
+struct ComplexData {
+    int id;
+    std::string name;
+    double value;
+
+    friend std::ostream& operator<<(std::ostream& os, const ComplexData& data) {
+        os << "ID: " << data.id << ", Name: " << data.name << ", Value: " << data.value;
+        return os;
+    }
+};
 
 int main() {
-    constexpr size_t poolSize = 1024;
-    constexpr size_t blockSize = 32;
+    constexpr size_t poolSize = 8192;
+    constexpr size_t blockSize = 64;
 
     FixedMemoryResource resource(poolSize, blockSize);
-    Stack<int> stack(&resource);
 
-    int n;
-    std::cout << "Enter numbers to push onto the stack (-1 to stop): ";
-    while (std::cin >> n && n != -1) {
-        stack.push(n);
+    // Create a stack for integers
+    Stack<int> intStack(&resource);
+    std::cout << "Pushing integers onto the stack. Enter -1 to stop:\n";
+    int number;
+    while (std::cin >> number && number != -1) {
+        intStack.push(number);
     }
 
     std::cout << "Stack contents (top to bottom): ";
-    for (auto it = stack.begin(); it != stack.end(); ++it) {
+    for (auto it = intStack.begin(); it != intStack.end(); ++it) {
         std::cout << *it << " ";
     }
     std::cout << "\n";
+
+    // Create a stack for complex data
+    Stack<ComplexData> complexStack(&resource);
+    std::cout << "\nPushing complex data onto the stack. Enter -1 as ID to stop:\n";
+    while (true) {
+        ComplexData data;
+        std::cout << "Enter ID: ";
+        std::cin >> data.id;
+        if (data.id == -1) break;
+        
+        std::cin.ignore();
+        std::cout << "Enter Name: ";
+        std::getline(std::cin, data.name);
+        std::cout << "Enter Value: ";
+        std::cin >> data.value;
+
+        complexStack.push(data);
+    }
+
+    std::cout << "Complex stack contents (top to bottom):\n";
+    for (auto it = complexStack.begin(); it != complexStack.end(); ++it) {
+        std::cout << *it << "\n";
+    }
 
     return 0;
 }
--- Lab_5/test/test.cpp
+++ Lab_5/test/test.cpp
@@ -1,35 +1,118 @@
 #include "../include/Stack.hpp"
 #include "../include/MemoryResource.hpp"
 #include <gtest/gtest.h>
+#include <string>
 
-TEST(StackTest, PushPopTest) {
-    constexpr size_t poolSize = 1024;
-    constexpr size_t blockSize = 32;
+struct ComplexData {
+    int id;
+    std::string name;
+    double value;
 
-    FixedMemoryResource resource(poolSize, blockSize);
-    Stack<int> stack(&resource);
+    bool operator==(const ComplexData& other) const {
+        return id == other.id && name == other.name && value == other.value;
+    }
+};
 
-    stack.push(10);
-    EXPECT_EQ(stack.top(), 10);
-    stack.pop();
-    EXPECT_TRUE(stack.empty());
+class StackTest : public ::testing::Test {
+protected:
+    constexpr static size_t poolSize = 8192;
+    constexpr static size_t blockSize = 64;
+    FixedMemoryResource resource;
+    Stack<int> intStack;
+    Stack<ComplexData> complexStack;
+
+    StackTest() : resource(poolSize, blockSize), intStack(&resource), complexStack(&resource) {}
+};
+
+// Test pushing and popping multiple integers
+TEST_F(StackTest, PushPopMultipleIntegers) {
+    for (int i = 0; i < 10; ++i) {
+        intStack.push(i);
+    }
+    
+    for (int i = 9; i >= 0; --i) {
+        EXPECT_EQ(intStack.top(), i);
+        intStack.pop();
+    }
+
+    EXPECT_TRUE(intStack.empty());
 }
 
-TEST(StackTest, IteratorTest) {
-    constexpr size_t poolSize = 1024;
-    constexpr size_t blockSize = 32;
+// Test pushing and popping complex data
+TEST_F(StackTest, PushPopComplexData) {
+    ComplexData data1{1, "Alpha", 10.5};
+    ComplexData data2{2, "Beta", 20.5};
+    ComplexData data3{3, "Gamma", 30.5};
 
-    FixedMemoryResource resource(poolSize, blockSize);
-    Stack<int> stack(&resource);
+    complexStack.push(data1);
+    complexStack.push(data2);
+    complexStack.push(data3);
 
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
+    EXPECT_EQ(complexStack.top(), data3);
+    complexStack.pop();
+    EXPECT_EQ(complexStack.top(), data2);
+    complexStack.pop();
+    EXPECT_EQ(complexStack.top(), data1);
+    complexStack.pop();
 
-    auto it = stack.begin();
-    EXPECT_EQ(*it, 3);
+    EXPECT_TRUE(complexStack.empty());
+}
+
+// Test boundary cases and exceptions
+TEST_F(StackTest, PopEmptyStackThrows) {
+    EXPECT_THROW(intStack.pop(), std::runtime_error);
+    EXPECT_THROW(complexStack.pop(), std::runtime_error);
+}
+
+// Test data handling (adjust for 8 Kb)
+TEST_F(StackTest, DataHandling) {
+    for (int i = 0; i < 100; ++i) {
+        intStack.push(i);
+    }
+
+    for (int i = 99; i >= 0; --i) {
+        EXPECT_EQ(intStack.top(), i);
+        intStack.pop();
+    }
+
+    EXPECT_TRUE(intStack.empty());
+}
+
+// Test iteration over complex data
+TEST_F(StackTest, IterateComplexData) {
+    ComplexData data1{1, "First", 123.45};
+    ComplexData data2{2, "Second", 678.90};
+    ComplexData data3{3, "Third", 42.42};
+
+    complexStack.push(data1);
+    complexStack.push(data2);
+    complexStack.push(data3);
+
+    auto it = complexStack.begin();
+    EXPECT_EQ(*it, data3);
     ++it;
-    EXPECT_EQ(*it, 2);
+    EXPECT_EQ(*it, data2);
     ++it;
-    EXPECT_EQ(*it, 1);
+    EXPECT_EQ(*it, data1);
+}
+
+// Test memory resource reuse
+TEST_F(StackTest, MemoryResourceReuse) {
+    for (int i = 0; i < 5; ++i) {
+        intStack.push(i);
+    }
+    for (int i = 0; i < 5; ++i) {
+        intStack.pop();
+    }
+
+    for (int i = 10; i < 15; ++i) {
+        intStack.push(i);
+    }
+
+    for (int i = 14; i >= 10; --i) {
+        EXPECT_EQ(intStack.top(), i);
+        intStack.pop();
+    }
+
+    EXPECT_TRUE(intStack.empty());
 }
